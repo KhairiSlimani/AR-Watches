650829c607204bb9a5322c644e0fa4a3ce58f472
Arij Ghazouani <62249967+arij-ghazouani123@users.noreply.github.com>
2023-10-24 15:34:59 +0100

    Controlling the window animation

--- Assets/WatchSelect.cs
+++ Assets/WatchSelect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class WatchSelect : MonoBehaviour
 {
@@ -12,9 +13,16 @@ public class WatchSelect : MonoBehaviour
     public GameObject w2Window;
     public GameObject w3Window;
 
+    Animation w1WindowAnimation;
+    Animation w2WindowAnimation;
+    Animation w3WindowAnimation;
+
     // Start is called before the first frame update
     void Start()
     {
+        w1WindowAnimation = w1Window.GetComponent<Animation>();
+        w2WindowAnimation = w2Window.GetComponent<Animation>();
+        w3WindowAnimation = w3Window.GetComponent<Animation>();
         
     }
 
@@ -27,6 +35,9 @@ public class WatchSelect : MonoBehaviour
         watchModel3.SetActive(false);
 
         // 2) Animate watch 1 window
+
+        w1WindowAnimation["w1animation"].speed = 1;
+        w1WindowAnimation.Play();
 
 
     }
@@ -41,6 +52,9 @@ public class WatchSelect : MonoBehaviour
 
         // 2) Animate watch 2 window
 
+        w2WindowAnimation["w2animation"].speed = 1;
+        w2WindowAnimation.Play();
+
     }
 
     public void WatchThreeButtonClicked(){
@@ -53,6 +67,38 @@ public class WatchSelect : MonoBehaviour
 
         // 2) Animate watch 3 window
 
+        w3WindowAnimation["w3animation"].speed = 1;
+        w3WindowAnimation.Play();
+
+    }
+
+    public void CloseButtonClicked()
+    {
+        string buttonName = EventSystem.current.currentSelectedGameObject.name;
+
+        if (buttonName == "w1close")
+        {
+            //play animation to close w1
+            w1WindowAnimation["w1animation"].speed = -1;
+            w1WindowAnimation["w1animation"].time = w1WindowAnimation["w1animation"].length;
+            w1WindowAnimation.Play();
+        }
+
+        else if (buttonName == "w2close")
+        {
+            //play animation to close w2
+            w2WindowAnimation["w2animation"].speed = -1;
+            w2WindowAnimation["w2animation"].time = w2WindowAnimation["w2animation"].length;
+            w2WindowAnimation.Play();
+        }
+
+        else if (buttonName == "w3close")
+        {
+            //play animation to close w3
+            w3WindowAnimation["w3animation"].speed = -1;
+            w3WindowAnimation["w3animation"].time = w3WindowAnimation["w3animation"].length;
+            w3WindowAnimation.Play();
+        }
     }
 
 
